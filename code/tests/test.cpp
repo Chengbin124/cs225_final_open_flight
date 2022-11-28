3e297a540b7bc1673c0e330ef5b6dff6525b0676
yifeikang <yifeik3@illinois.edu>
2022-11-28 22:06:54 +0000

    Add test data, update some test

--- code/tests/test.cpp
+++ code/tests/test.cpp
@@ -4,26 +4,25 @@
 
 using namespace std;
 
-TEST_CASE("Airport distance", "[weight=1][part=1]") {
-    Airport a = {10,20,"1","a"}; 
-    Airport b = {10,30,"2","b"};
-    Airport c = {0,40,"3","c"};
+TEST_CASE("Airport distance", "[weight=1][part=1]")
+{
+    Airport a = {10, 20, "1", "a"};
+    Airport b = {10, 30, "2", "b"};
+    Airport c = {0, 40, "3", "c"};
     double a2b = 600;
     double a2c = 1305;
     double b2c = 774;
-    REQUIRE(a.calculateDistance(b)==a2b);
-    REQUIRE(a.calculateDistance(c)==a2c);
-    REQUIRE(b.calculateDistance(c)==b2c);
+    REQUIRE(a.calculateDistance(b) == a2b);
+    REQUIRE(a.calculateDistance(c) == a2c);
+    REQUIRE(b.calculateDistance(c) == b2c);
 }
 
-// TEST_CASE("Graph constructor", "[weight=1][part=1]") {
-//     cout<<"need add route file to test!"<<endl;
-//     cout<<"also need function to get all airports"<<endl;
-//     Graph() g;
-//     g = Graph("sample_airports.dat.txt","routfile");
-//     REQUIRE(g.vertices) = sample_airports.size();
-
-// }
+TEST_CASE("Graph constructor", "[weight=1][part=1]")
+{
+    Graph g;
+    g = Graph("../../data/test_airport.txt", "../../data/test_route.txt");
+    // REQUIRE(g.vertices) = sample_airports.size();
+}
 
 // TEST_CASE("PUT YOUR TESTS HERE!!!", "[tag1][mytest]") {
 //     // Feel free to use this to put your own test cases.
