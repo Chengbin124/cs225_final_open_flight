caf5df8294aa48bbe496a1376000839ae3cb8449
yifeikang <yifeik3@illinois.edu>
2022-11-29 04:38:14 +0000

    Finish test case for graph constructor

--- code/src/graph.h
+++ code/src/graph.h
@@ -7,100 +7,109 @@
 #include <iostream>
 #include <fstream>
 
-
 using namespace std;
 
-class Graph {
-    public:
-        void readFromAirports(string file);
+class Graph
+{
+public:
+    void readFromAirports(string file);
 
-        void readFromRoutes(string file);
+    void readFromRoutes(string file);
 
-        /** 
-         * Constructor of the graph, taking datas from two datasets.
-         */ 
-        Graph(string airports, string routes);
+    /**
+     * Constructor of the graph, taking datas from two datasets.
+     */
+    Graph(string airports, string routes);
 
-        Graph();
+    Graph();
 
-        /**
-         * Implement Dijkstra's algorithm to get the shortest paths from one point.
-         * @param source The start point in the graph.
-         * @return The shortest paths from the start points to all points, stored in a sequence. 
-         * To be modified in next part so that it will find all the shortest paths.
-         */
-        vector<vector<int>> shortestPaths(int source);
+    /**
+     * Implement Dijkstra's algorithm to get the shortest paths from one point.
+     * @param source The start point in the graph.
+     * @return The shortest paths from the start points to all points, stored in a sequence.
+     * To be modified in next part so that it will find all the shortest paths.
+     */
+    vector<vector<int>> shortestPaths(int source);
 
-        /**
-         * Find the shortest paths from one city to another via airports.
-         */
-        vector<string> shortestPaths(string s1, string s2);
+    /**
+     * Find the shortest paths from one city to another via airports.
+     */
+    vector<string> shortestPaths(string s1, string s2);
 
-        /**
-         * Modified the Dijkstra's algorithm to get all shortest paths from one point to all other points.
-         */
-        map<int, vector<vector<int>>> allShortestPaths(int source);
+    /**
+     * Modified the Dijkstra's algorithm to get all shortest paths from one point to all other points.
+     */
+    map<int, vector<vector<int>>> allShortestPaths(int source);
 
-        /**
-         * Calaculate the number of shortest paths from s1 to s2.
-         */
-        int countShortestPaths(string s1, string s2);
+    /**
+     * Calaculate the number of shortest paths from s1 to s2.
+     */
+    int countShortestPaths(string s1, string s2);
 
-        /**
-         * Calculate the number of shortest paths from s1 to s2, going through s3.
-         */
-        int countShortestPaths(string s1, string s2, string s3);
+    /**
+     * Calculate the number of shortest paths from s1 to s2, going through s3.
+     */
+    int countShortestPaths(string s1, string s2, string s3);
 
-        /**
-         * Calculate the betweenness centrality of one point.
-         */
-        double betweennessCentrality(string s);
+    /**
+     * Calculate the betweenness centrality of one point.
+     */
+    double betweennessCentrality(string s);
 
-        /**
-         * Using BFS to traverse the graph.
-         */
-        vector<int> Bfs(int start) const;
+    /**
+     * Using BFS to traverse the graph.
+     */
+    vector<int> Bfs(int start) const;
 
-    private:
-        vector<string> split(string s);
-        void printAirports(){
-            int counter = 0;
-            map<int,string>::iterator it;
-            for (it = convert.begin(); it != convert.end(); it++)
+    /*helper function for test cases*/
+    /*return the total number of vertices*/
+    int verticeCount();
+    /*return the adjacency list of an airport id*/
+    vector<string> getAdjacency(string id);
+
+private:
+    vector<string> split(string s);
+    void printAirports()
+    {
+        int counter = 0;
+        map<int, string>::iterator it;
+        for (it = convert.begin(); it != convert.end(); it++)
+        {
+            cout << it->second << endl;
+            counter++;
+        }
+        cout << counter << " " << vertices.size() << endl;
+    }
+    void printEdges()
+    {
+        int count = 0;
+
+        for (unsigned i = 0; i < vertices.size(); i++)
+        {
+            if (adjacency[i].size() > 1)
             {
-                cout << it->second << endl;
-                counter++;
-            }
-            cout << counter<<" "<<vertices.size() << endl;
-        }
-        void printEdges(){
-            int count = 0;
-            
-            for (unsigned i = 0; i < vertices.size(); i++) {
-                if (adjacency[i].size() >1 ){
-                    cout << "Airport " << convert[i] << " has flight to ";
-                    for(unsigned j = 0 ; j < adjacency[i].size();j++){
-                        cout << convert[adjacency[i][j]] << ", ";
-                        count++;
-                    }
-                cout << endl;
+                cout << "Airport " << convert[i] << " has flight to ";
+                for (unsigned j = 0; j < adjacency[i].size(); j++)
+                {
+                    cout << convert[adjacency[i][j]] << ", ";
+                    count++;
                 }
-                
+                cout << endl;
             }
-            cout<<count<<endl;
         }
-        /**
-         * Find the corresponding airport id/name to the index in the graph.
-         */
-        map<string, int> vertices;
+        cout << count << endl;
+    }
+    /**
+     * Find the corresponding airport id/name to the index in the graph.
+     */
+    map<string, int> vertices;
 
-        map<int, Airport> airports;
+    map<int, Airport> airports;
 
-        map<int, string> convert;
+    map<int, string> convert;
 
-        vector<vector<int>> adjacency;
+    vector<vector<int>> adjacency;
 
-        //first item in row is current airport, every other item is each airport it is connected to
-        vector<map<int,int>> weights;
+    // first item in row is current airport, every other item is each airport it is connected to
+    vector<map<int, int>> weights;
 };
-
--- code/tests/test.cpp
+++ code/tests/test.cpp
@@ -4,24 +4,34 @@
 
 using namespace std;
 
-TEST_CASE("Airport distance", "[weight=1][part=1]")
-{
-    Airport a = {10, 20, "1", "a"};
-    Airport b = {10, 30, "2", "b"};
-    Airport c = {0, 40, "3", "c"};
-    double a2b = 600;
-    double a2c = 1305;
-    double b2c = 774;
-    REQUIRE(a.calculateDistance(b) == a2b);
-    REQUIRE(a.calculateDistance(c) == a2c);
-    REQUIRE(b.calculateDistance(c) == b2c);
-}
+// TEST_CASE("Airport distance", "[weight=1][part=1]")
+// {
+//     Airport a = {10, 20, "1", "a"};
+//     Airport b = {10, 30, "2", "b"};
+//     Airport c = {0, 40, "3", "c"};
+//     double a2b = 600;
+//     double a2c = 1305;
+//     double b2c = 774;
+//     REQUIRE(a.calculateDistance(b) == a2b);
+//     REQUIRE(a.calculateDistance(c) == a2c);
+//     REQUIRE(b.calculateDistance(c) == b2c);
+// }
 
 TEST_CASE("Graph constructor", "[weight=1][part=1]")
 {
-    Graph g;
-    g = Graph("../../data/test_airport.txt", "../../data/test_route.txt");
-    // REQUIRE(g.vertices) = sample_airports.size();
+    Graph graph;
+    graph = Graph("../../data/test_airport.txt", "../../data/test_route.txt");
+    int airports = graph.verticeCount();
+    REQUIRE(airports == 10);
+    // Airport a = {10, 100, "1", "a"};
+    // Airport e = {50, 140, "5", "e"};
+    // Airport g = {-20, 20, "7", "g"};
+
+    REQUIRE(graph.getAdjacency("1").size() == 1);
+    REQUIRE(graph.getAdjacency("1")[0] == "2");
+    REQUIRE(graph.getAdjacency("5").size() == 0);
+    REQUIRE(graph.getAdjacency("7").size() == 5);
+    REQUIRE(graph.getAdjacency("7")[3] == "6");
 }
 
 // TEST_CASE("PUT YOUR TESTS HERE!!!", "[tag1][mytest]") {
